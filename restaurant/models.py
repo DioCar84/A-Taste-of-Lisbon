558e3101c8123d7e971ede8fa4bc9105a3f89ece
Diogo Carvalho <dcar84@gmail.com>
2022-07-26 15:06:48 +0000

    Add Reservation model to DB

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    total_people = models.IntegerField()
+    date = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.last_name + " " + self.date
+
+    class Meta:
+        ordering = ['-date']
