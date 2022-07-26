4df08808b0b81dc902f29e89525ab07b4e763c40
Diogo Carvalho <dcar84@gmail.com>
2022-07-26 16:43:05 +0000

    Add view and url path for reservations page

--- restaurant/views.py
+++ restaurant/views.py
@@ -6,3 +6,6 @@ def home(request):
     hero_image = Photo.objects.get(title='Lisbon Tram');
     context = {'hero_image': hero_image, }
     return render(request, 'restaurant/home.html', context)
+
+def reservations(request):
+    return render(request, 'restaurant/reservations.html')
