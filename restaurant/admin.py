361fb12774521e23ce85cd5f77b8e2121117ae03
Diogo Carvalho <dcar84@gmail.com>
2022-07-26 22:35:57 +0000

    Display menu items from DB and style them

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Reservation, Photo
+from .models import Reservation, Photo, Menu
 
 # Register your models here.
 admin.site.register(Reservation)
 admin.site.register(Photo)
+admin.site.register(Menu)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-from .models import Reservation, Photo
+from django.core.paginator import Paginator
+from .models import Reservation, Photo, Menu
 
 # Create your views here.
 def home(request):
-    hero_image = Photo.objects.get(title='Lisbon Tram');
+    hero_image = Photo.objects.get(title='Lisbon Tram')
     context = {'hero_image': hero_image, }
     return render(request, 'restaurant/home.html', context)
 
 def menu(request):
-    return render(request, 'restaurant/menu.html')
+    menu = Menu.objects.all()
+    paginator = Paginator(menu, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {'menu': menu, 'page_obj': page_obj,}
+
+    return render(request, 'restaurant/menu.html', context)
 
 def reservations(request):
     return render(request, 'restaurant/reservations.html')
