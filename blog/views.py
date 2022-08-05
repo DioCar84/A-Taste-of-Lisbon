3f1bf2ea38ef51c4c9b932a8cab6ebb50da5216a
Diogo Carvalho <dcar84@gmail.com>
2022-08-05 15:55:17 +0000

    Add content and style to blog home

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from restaurant.models import Photo 
 
 # Create your views here.
 def blog(request):
-    return render(request, 'blog/blog_home.html')
+    blog_image = Photo.objects.get(title='Blog Test')
+    context = {'blog_image': blog_image, }
+    return render(request, 'blog/blog_home.html', context)
