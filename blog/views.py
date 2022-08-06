ca936282aa3cf2b00d0ea0bb8b8d9fb3dd77b0d6
Diogo Carvalho <dcar84@gmail.com>
2022-08-06 00:01:45 +0000

    Add DB querys to blog template

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
+from django.db.models import Count
 from .models import Post, Comment
 from .forms import PostForm
 
 # Create your views here.
 def blog(request):
+    Post.objects.annotate(Count('comments'))
     posts = Post.objects.all()
     dish_type = PostForm
 
