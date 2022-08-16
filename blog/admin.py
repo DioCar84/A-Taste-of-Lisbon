18eec047ce84edce24f4dbcb59a32365993b1305
Diogo Carvalho <dcar84@gmail.com>
2022-08-16 08:21:28 +0000

    Add docstrings to various classes and functions

--- blog/admin.py
+++ blog/admin.py
@@ -5,14 +5,22 @@ from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    """
+    The PostAdmin class will add the Post model to admin page.
+    Also defines the content field as a Summernote field to use the text editor,
+    as well as prepopulating the slug based on the post title.
+    """
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
+    """
+    The CommentAdmin class will add the Comment model to admin page.
+    Also allows the admin to approve comments and,
+    to filter the comments objects by their approved status.
+    """
     list_filter = ('approved',)
     actions = ['approve_comments']
 
--- users/apps.py
+++ users/apps.py
@@ -2,6 +2,10 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """
+    The UsersConfig class defines the django configuration for the users app.
+    Also allows the use of signals by importing them.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
